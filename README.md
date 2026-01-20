241b23b05c6f711cb90c33c44b26a2fee115fd07
kyle <zhendong.li@edoctor.cn>
2026-01-20 17:34:26 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,93 @@
+## 概览
+
+一个简单的微信授权工具
+
+## 环境要求
+
+支持的环境如下：
+
++ PHP >= 8.0
+
+## 安装
+
+推荐使用 PHP 包管理工具 [Composer](https://getcomposer.org/) 安装 SDK：
+
+```shell
+composer require module-wechat/oauth
+```
+
+## 快速使用
+以下代码示例向您展示了助手的调用方法：
+
+```php
+<?php
+
+require 'vendor/autoload.php';
+use ModuleWechat\Oauth\WechatOauthServer;
+
+// .env 中配置appid和secret
+WECHAT_APPID=appid
+WECHAT_SECRET=secret
+
+$wechat = new WechatOauthServer();
+
+// 获取收取url scope 默认 snsapi_base
+$wechat->getAuthUrl('redirect_uri','scope');
+
+// 获取授权access_token
+$wechat->getOauthToken('code');
+// 返回示例
+{
+  "access_token": "ACCESS_TOKEN",
+  "expires_in": 7200,
+  "refresh_token": "REFRESH_TOKEN",
+  "openid": "OPENID",
+  "unionid": "UNIONID",
+  "is_snapshotuser": 1
+}
+
+// 获取用户信息
+$wechat->getUserInfo('openid','access_token');
+// 返回示例
+{   
+  "openid": "OPENID",
+  "nickname": NICKNAME,
+  "sex": 1,
+  "province":"PROVINCE",
+  "city":"CITY",
+  "country":"COUNTRY",
+  "headimgurl":"https://thirdwx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46",
+  "privilege":[ "PRIVILEGE1" "PRIVILEGE2"     ],
+  "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+}
+
+// 获取基础access_token
+$wechat->getAccessToken();
+// 返回示例
+{
+  "access_token": "ACCESS_TOKEN",
+  "expires_in": 7200
+}
+
+// 获取jssdk的临时票据 ticket
+$wechat->getTicket('access_token');
+// 返回示例
+{
+  "ticket": "ticket",
+  "expires_in": 7200,
+  "errcode": "errcode",
+  "errmsg": "errmsg"
+}
+
+// 获取jssdk的配置
+$wechat->getJssdk('url','ticket');
+// 返回示例
+{
+  "nonceStr": "nonceStr",
+  "appId": "appId",
+  "timestamp": "timestamp",
+  "signature": "signature",
+  "url": "url"
+}
+
+```
---
+++ src/WechatOauthServer.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace ModuleWechat\Oauth;
+
+use ModuleWechat\Common\Server;
+
+class WechatOauthServer extends Server
+{
+    /**
+     * 获取授权跳转地址
+     * @param $redirect_uri
+     * @param $scope
+     * @param $state
+     * @return string
+     */
+    public function getAuthUrl($redirect_uri, $scope = 'snsapi_base', $state = 'STATE')
+    {
+        $appid = $this->config->getAppid();
+        return sprintf('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=%s#wechat_redirect',$appid,$redirect_uri,$scope,$state);
+    }
+
+    /**
+     * 换取用户授权凭证
+     * @param $code
+     * @return mixed
+     */
+    public function getOauthToken($code)
+    {
+        return $this->http->request('sns/oauth2/access_token',[
+            'appid' => $this->config->getAppid(),
+            'secret' => $this->config->getSecret(),
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+
+    /**
+     * 获取授权用户信息
+     * @param $openid
+     * @param $access_token
+     * @return mixed
+     */
+    public function getUserInfo($openid, $access_token)
+    {
+        return $this->http->request('sns/userinfo',[
+            'access_token' => $access_token,
+            'openid' => $openid
+        ]);
+    }
+
+    /**
+     * JS-SDK配置
+     * @param $url
+     * @param $ticket
+     * @return array
+     */
+    public function getJssdk($url, $ticket)
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $nonce = substr(str_shuffle($pool),0,10);
+        $time = time();
+        $str = sprintf('jsapi_ticket=%s&noncestr=%s&timestamp=%s&url=%s',$ticket,$nonce,$time,$url);
+        return [
+            'nonceStr' => $nonce,
+            'appId' => $this->config->getAppid(),
+            'timestamp' => $time,
+            'signature' => sha1($str),
+            'url' => $url
+        ];
+    }
+}
